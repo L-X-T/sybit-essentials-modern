d4f07b736ef58ee95e7bb8d28869450b44bc0c51
Alex Thalhammer <alex@angular-software.at>
2021-06-29 17:32:53 +0200

    Updates, config best practices, switch to Jest & Cypress and other improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule,
-   ],
-   declarations: [
-      AppComponent,
-      SidebarComponent,
-      NavbarComponent
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, NavbarComponent, SidebarComponent],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
