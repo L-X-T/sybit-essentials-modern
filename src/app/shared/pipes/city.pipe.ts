63c5582657760f83c34a864eca5db18fa611804f
Alex Thalhammer <alex@angular-software.at>
2021-06-30 08:37:25 +0200

    1st Angular Pipe

---
+++ src/app/shared/pipes/city.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'city',
+  standalone: true,
+})
+export class CityPipe implements PipeTransform {
+  transform(value: string, format?: string): string {
+    let short, long;
+
+    switch (value) {
+      case 'Graz':
+        short = 'GRZ';
+        long = 'Flughafen Graz Thalerhof';
+        break;
+
+      case 'Hamburg':
+        short = 'HAM';
+        long = 'Airport Hamburg Fulsbüttel Helmut Schmidt';
+        break;
+
+      case 'Wien':
+        short = 'VIE';
+        long = 'Flughafen Wien Schwechat';
+        break;
+
+      default:
+        short = long = value;
+    }
+
+    if (format === 'short') {
+      return short;
+    }
+
+    return long;
+  }
+}
