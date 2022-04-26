53119f3b6f2b8f9fc2ad749d218c7010651a9f3c
Alexander Thalhammer <alex@angular-software.at>
2022-04-26 16:44:34 +0200

    Demo: add observable/subscription/async pipe demo

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,42 +1,64 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { Flight } from '../entities/flight';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FlightService } from './flight.service';
-import { AsyncCityPipe } from '../shared/pipes/async-city.pipe';
+import { CityPipe } from '../shared/pipes/city.pipe';
+import { Observable, Observer, share, Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, FormsModule, AsyncCityPipe],
+  imports: [CommonModule, FormsModule, CityPipe],
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrl: './flight-search.component.css',
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnDestroy {
   from = 'Hamburg';
   to = 'Graz';
   flights: Flight[] = [];
+  flights$?: Observable<Flight[]>;
+  flightsSubscription?: Subscription;
+  private readonly onDestroySubject = new Subject<void>();
+  readonly terminator$ = this.onDestroySubject.asObservable();
+
   selectedFlight?: Flight;
 
   message = '';
 
-  private readonly flightService = inject(FlightService);
-  // constructor(private readonly flightService: FlightService) {}
+  // private readonly flightService = inject(FlightService);
+  constructor(private readonly flightService: FlightService) {
+    this.onSearch();
+  }
 
   onSearch(): void {
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
-        this.flights = flights;
-      },
-      error: (errResp: HttpErrorResponse) => {
-        console.error('Error loading flights', errResp);
-      },
-      complete: () => {
-        console.debug('Flights loading completed.');
-      },
-    });
+    // 1. my observable
+    this.flights$ = this.flightService.find(this.from, this.to).pipe(share());
+
+    // 2. my observer
+    const flightsObserver: Observer<Flight[]> = {
+      next: (flights) => (this.flights = flights),
+      error: (errResp: HttpErrorResponse) => console.error('Error loading flights', errResp),
+      complete: () => console.debug('Flights loading completed.'),
+    };
+
+    // 3a. my subscription
+    this.flightsSubscription?.unsubscribe();
+    this.flightsSubscription = this.flights$.subscribe(flightsObserver);
+
+    // 3b. takeUntil terminator$ emits
+    this.flights$.pipe(takeUntil(this.terminator$)).subscribe(flightsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // 4a. my unsubscribe
+    this.flightsSubscription?.unsubscribe();
+
+    // 4b. subject emit thru terminator$
+    this.onDestroySubject.next();
+    this.onDestroySubject.complete();
   }
 
   onSelect(selectedFlight: Flight): void {
