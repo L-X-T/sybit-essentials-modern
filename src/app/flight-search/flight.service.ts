322ff977252d5315ac496b8ed29492de40f929be
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-09-11 14:51:29 +0200

    1st Angular Service

---
+++ src/app/flight-search/flight.service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { Flight } from '../entities/flight';
+
+@Injectable({ providedIn: 'root' })
+export class FlightService {
+  private readonly headers = new HttpHeaders().set('Accept', 'application/json');
+  private readonly url = 'http://www.angular.at/api/flight';
+  // private readonly url = 'https://demo.angulararchitects.io/api/Flight';
+
+  private readonly http = inject(HttpClient);
+  // constructor(private readonly http: HttpClient) {}
+
+  find(from: string, to: string): Observable<Flight[]> {
+    const params = new HttpParams().set('from', from).set('to', to);
+    return this.http.get<Flight[]>(this.url, { headers: this.headers, params });
+  }
+
+  save(flight: Flight): Observable<Flight> {
+    return this.http.post<Flight>(this.url, flight, { headers: this.headers });
+  }
+}
