0942a773770b830f263402dd0c7c86f01c879fa7
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-02-28 12:06:45 +0100

    Add airports component

---
+++ src/app/airports/airports.component.ts
@@ -0,0 +1,14 @@
+import { Component, inject } from '@angular/core';
+import { delay } from 'rxjs';
+import { AirportService } from './airport.service';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-airports',
+  templateUrl: './airports.component.html',
+  imports: [AsyncPipe],
+})
+export class AirportsComponent {
+  readonly airports$ = inject(AirportService).findAll().pipe(delay(3_000));
+}
