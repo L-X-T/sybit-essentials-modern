eb4757f56dbf7d5997c0f54f217ce0852fa2bd2f
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-02-28 12:06:45 +0100

    Add airports component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { AirportsComponent } from './airports/airports.component';
 
 @Component({
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent],
+  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent, AirportsComponent],
   selector: 'app-flight-app',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
