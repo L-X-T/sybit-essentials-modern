6dfa67f43d35acfbb90ed02e2a57a7bf754b1d7d
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-08-21 11:32:00 +0200

    Bonus: Async Pipe

---
+++ src/app/airports/airport.service.ts
@@ -0,0 +1,27 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AirportNameFormat } from '../entities/airport-name-format';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AirportService {
+  private readonly url = 'http://www.angular.at/api/airport';
+
+  private readonly httpClient = inject(HttpClient);
+
+  findAll(): Observable<string[]> {
+    return this.httpClient.get<string[]>(this.url);
+  }
+
+  getAirportName(name: string, format: AirportNameFormat): Observable<string> {
+    let url = 'http://angular-at.azurewebsites.net/api/airport/';
+    url += format === 'short' ? 'code' : 'fullName';
+
+    const params = new HttpParams().set('name', name);
+
+    return this.httpClient.get<string>(url, { params });
+  }
+}
