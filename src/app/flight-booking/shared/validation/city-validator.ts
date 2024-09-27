c64237562845eae699516772c3be2d4f33b5dc86
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-09-27 09:38:09 +0200

    [RF] Parametrizable validator

--- src/app/flight-booking/shared/validation/city-validator.ts
+++ src/app/flight-booking/shared/validation/city-validator.ts
@@ -1,15 +1,17 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function validateCity(c: AbstractControl): ValidationErrors | null {
-  const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
-  if (c.value && !validCities.includes(c.value)) {
-    return {
-      city: {
-        actualCity: c.value,
-        validCities: validCities.join(', ')
-      }
-    };
-  }
+export function validateCity(validCities: string[]): ValidatorFn {
+  // const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
+  return (c: AbstractControl): ValidationErrors | null => {
+    if (c.value && !validCities.includes(c.value)) {
+      return {
+        city: {
+          actualCity: c.value,
+          validCities: validCities.join(', '),
+        },
+      };
+    }
 
-  return null;
+    return null;
+  };
 }
