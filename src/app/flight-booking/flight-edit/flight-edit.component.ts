80ebb5d31a95af011deec4760c8178fe64caf1af
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-09-27 09:20:37 +0200

    [RF] Using Angular Validators

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,17 +1,18 @@
 import { Component, DestroyRef, inject, Input, OnChanges } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { Flight } from '../../entities/flight';
 import { FlightService } from '../shared/services/flight.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FlightValidationErrorsComponent } from '../flight-validation-errors/flight-validation-errors.component';
 
 @Component({
   standalone: true,
   selector: 'app-flight-edit',
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css'],
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FlightValidationErrorsComponent],
 })
 export class FlightEditComponent implements OnChanges {
   @Input() flight?: Flight | null;
@@ -20,10 +21,26 @@ export class FlightEditComponent implements OnChanges {
   private readonly flightService = inject(FlightService);
 
   editForm: FormGroup = inject(FormBuilder).group({
-    id: [0],
-    from: [''],
-    to: [''],
-    date: [''],
+    id: [0, [Validators.required, Validators.min(0), Validators.pattern(/^[0-9]+$/)]],
+    from: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        Validators.pattern(/^[a-zA-ZäöüÄÖÜß ]+$/),
+      ],
+    ],
+    to: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15),
+        Validators.pattern(/^[a-zA-ZäöüÄÖÜß ]+$/),
+      ],
+    ],
+    date: ['', [Validators.required, Validators.minLength(33), Validators.maxLength(33)]],
   });
 
   message = '';
