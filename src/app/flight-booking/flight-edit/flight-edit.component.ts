87e7ae86f32753ac6061f17e64ed9201022eda99
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-09-27 11:48:08 +0200

    [Routing] Bonus: Programatic routing

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, DestroyRef, EventEmitter, inject, Input, OnChanges, Output }
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 import { Flight } from '../../entities/flight';
@@ -31,6 +31,7 @@ export class FlightEditComponent implements OnChanges {
   private readonly destroyRef = inject(DestroyRef);
   private readonly flightService = inject(FlightService);
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
 
   editForm: FormGroup = inject(FormBuilder).group(
     {
@@ -100,6 +101,8 @@ export class FlightEditComponent implements OnChanges {
           this.flight = flight;
           this.message = 'Success saving!';
           this.patchFormValue();
+
+          setTimeout(() => this.router.navigate(['/flight-booking/flight-search']), 3000);
         },
         error: (err: HttpErrorResponse) => {
           if (this.debug) {
